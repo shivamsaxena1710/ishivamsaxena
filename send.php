fb16b4b0c52e098a1a4c30d6fc2cc086a0e810b5
shivamsaxena1710 <shivamsaxena1710@gmail.com>
2017-08-13 21:42:49 +0530

    Website upload 13th August

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$first_name = $_POST['name'];
+$email = $_POST['email'];
+$sub = $_POST['Subject'];
+$message = $_POST['message'];
+$email_from = 'info@ishivamsaxena.ml';
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $first_name.\n".
+    "Here is the message:\n $email $sub $message".
+$to = "shivambhushan1717@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: index.html');
+?>
