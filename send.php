89b56f7d38d23666ac4e2c453e1c4009bc76b487
shivamsaxena1710 <shivamsaxena1710@gmail.com>
2017-09-01 23:57:23 +0530

    email

--- send.php
+++ send.php
@@ -7,11 +7,11 @@ $email_from = 'info@ishivamsaxena.ml';
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $first_name.\n".
     "Here is the message:\n $email $sub $message".
-$to = "shivambhushan1717@gmail.com";
+$to = "shivamsaxena1710@gmail.com";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: index.html');
+header('Location: ishivamsaxena.ml');
 ?>
